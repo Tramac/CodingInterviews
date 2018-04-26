3646dc51e1e6b00c9e0d0dd6eee85ab689e6d25a
Tramac <1145955920@qq.com>
2018-04-26 21:57:13 +0800

    Create main.cpp

---
+++ 14_ReorderArray/main.cpp
@@ -0,0 +1,22 @@
+# include <stdio.h>
+
+void ReorderOddEven(int* pData, unsigned int length)
+{
+	if (pData == NULL || length == 0)
+		return;
+	int* pBegin = pData;
+	int* pEnd = pData + length - 1;
+	while (pBegin < pEnd)
+	{
+		while (pBegin < pEnd && (*pBegin & 0x1) != 0)
+			pBegin++;
+		while (pBegin < pEnd && (*pEnd & 0x1) == 0)
+			pEnd--;
+		if (pBegin < pEnd)
+		{
+			int temp = *pBegin;
+			*pBegin = *pEnd;
+			*pEnd = temp;
+		}
+	}
+}
